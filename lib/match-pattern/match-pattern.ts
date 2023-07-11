cf31850dff2ecd4ed68fcb369f859581fc07e36b
Mishieck <mishieckmwale@gmail.com>
2023-07-11 17:51:20 +0200

    feat: make 'matchPattern' generic - Make  accept a type parameter for the expected result type.

--- lib/match-pattern/match-pattern.ts
+++ lib/match-pattern/match-pattern.ts
@@ -2,9 +2,9 @@ import match, { type Entry } from '../match/match.ts';
 import { getMatcher, type PatternEntry } from './helpers.ts';
 
 const matchPattern =
-  (value: unknown) =>
+  <Result = unknown>(value: unknown) =>
   (...entries: Array<PatternEntry>) =>
-    match(value)(
+    match<Result>(value)(
       ...entries.map(([pattern, handle]) => {
         const [isMatch, getValue] = getMatcher(pattern);
         console.log({ pattern, isMatch, getValue });
