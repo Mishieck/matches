526a93647d1bf0aced74a14aad8bf7ca5d84fd66
Mishieck <mishieckmwale@gmail.com>
2023-07-17 20:14:59 +0200

    docs(match): add JSDocs for 'hasProperty' helper

--- lib/match/helpers.ts
+++ lib/match/helpers.ts
@@ -608,6 +608,18 @@ export const none: IsMatch<ReturnType<IsMatch>, Iterable<unknown>> =
     return true;
   };
 
+/**
+ * Checks if a `Record` has a particular property.
+ *
+ * @param property - The property to check.
+ * @returns - a function that takes a `Record` as an argument and returns a
+ *   boolean that indicates whether the `property` exists on the record or
+ *   not.
+ * @example
+ * ```ts
+ * hasProperty('name')({ name: 'match' }); // true
+ * ```
+ */
 export const hasProperty: IsMatch<string, GenericRecord> =
   property => object => {
     return property in object;
