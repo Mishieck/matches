b7f598d1875c29a903e41021dabae0f703592e94
Mishieck <mishieckmwale@gmail.com>
2025-06-14 22:30:34 +0200

    test: add package tests

---
+++ mod_test.ts
@@ -0,0 +1,16 @@
+import { describe } from "@std/testing";
+import * as M from "jsr:@mishieck/matches@0.2.2";
+
+describe("mod", () => {
+  M.equals(1)(1); // true
+  M.equals(1)(2); // false
+
+  M.isArray()([]); // true
+  M.isArray()({}); // false
+
+  M.contains(1)([1, 2]); // true
+  M.contains(1)([2, 3]); // false
+
+  M.not(M.equals(1))(2); // true
+  M.not(M.equals(1))(1); // false
+});
